264a9ea7cefebd8f8d496624994bd09571c42acc
NS011618 <96785237+NS011618@users.noreply.github.com>
2023-10-02 00:06:09 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,36 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-void main()
-{
-    char str[]="Hello World";
-    char str1[11];
-    char str2[11];
-    char str3[11];
-    int i,len;
-    len = strlen(str);
-    printf("The Plain Text: ");
-    for (i=0;i<len;i++)
-    {
-        printf("%c", str[i]);
-    }
-    printf("\nCipher text after AND Operation: ");
-    for(i=0;i<len;i++)
-    {
-        str1[i] = str[i]&127;
-        printf("%c",str1[i]);
-    }
-    printf("\nCipher text XOR Operation: ");
-    for(i=0;i<len;i++)
-    {
-        str3[i] = str[i]^127;
-        printf("%c",str3[i]);
-    }
-    printf("\nCipher text OR Operation: ");
-    for(i=0;i<len;i++)
-    {
-        str2[i] = str[i]|127;
-        printf("%c",str2[i]);
-    }
-    printf("\n");
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string s="Hello world";
+string ans1="";
+string ans2="";
+string ans3="";
+for(auto it : s){
+    ans1+=(it&127);
+    
+}
+for(auto it : s){
+    ans2+=(it|127);
+
+}
+for(auto it : s){
+    ans3+=(it^127);
+
+}
+cout<<"AND "<<ans1<<endl;
+cout<<"OR "<<ans2<<endl;
+cout<<"XOR "<<ans3<<endl;
+
 }
